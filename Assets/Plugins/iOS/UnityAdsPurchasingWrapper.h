79d8ad30b60e07c62e4cea3e24f20c1b4ccb8290
Diego Offerni <drofferni@gmail.com>
2019-02-06 23:39:05 -0800

    fixing last commit

--- Assets/Plugins/iOS/UnityAdsPurchasingWrapper.h
+++
@@ -1,8 +0,0 @@
-extern "C" {
-    typedef void (*UnityAdsPurchasingDidInitiatePurchasingCommandCallback)(const char * eventString);
-    typedef void (*UnityAdsPurchasingGetProductCatalogCallback)();
-    typedef void (*UnityAdsPurchasingGetPurchasingVersionCallback)();
-    typedef void (*UnityAdsPurchasingInitializeCallback)();
-}
-
-void InitializeUnityAdsPurchasingWrapper();
